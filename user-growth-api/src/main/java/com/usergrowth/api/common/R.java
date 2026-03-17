51feafe54e7b755ba4d6ed76a0c77b6b1c2a6356
TianTC <3042752220@qq.com>
2026-03-17 10:34:30 +0800

    统一响应包装类

---
+++ user-growth-api/src/main/java/com/usergrowth/api/common/R.java
@@ -0,0 +1,37 @@
+package com.usergrowth.api.common;
+
+
+import lombok.Data;
+
+@Data
+public class R<T>  {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> R<T> ok(T data) {
+        R<T> r = new R<>();
+        r.code = 200;
+        r.message = "success";
+        r.data = data;
+        return r;
+    }
+
+    public static <T> R<T> fail(String message) {
+        R<T> r = new R<>();
+        r.code = 500;
+        r.message = message;
+        r.data = null;
+        return r;
+    }
+
+    public static <T> R<T> fail(int code, String message) {
+        R<T> r = new R<>();
+        r.code = code;
+        r.message = message;
+        r.data = null;
+        return r;
+    }
+
+}
