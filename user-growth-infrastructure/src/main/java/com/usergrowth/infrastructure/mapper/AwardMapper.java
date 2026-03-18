97893e6e691269d51fae2d350cb047ff4f0a051c
TianTC <3042752220@qq.com>
2026-03-18 14:21:28 +0800

    新增库存初始化接口

--- user-growth-infrastructure/src/main/java/com/usergrowth/infrastructure/mapper/AwardMapper.java
+++ user-growth-infrastructure/src/main/java/com/usergrowth/infrastructure/mapper/AwardMapper.java
@@ -3,7 +3,14 @@ package com.usergrowth.infrastructure.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.usergrowth.infrastructure.po.AwardPO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface AwardMapper extends BaseMapper<AwardPO> {
+
+    @Select("SELECT * FROM award WHERE allow_oversell = 1 AND status = 1")
+    List<AwardPO> selectOversellAwards();
+
 }
