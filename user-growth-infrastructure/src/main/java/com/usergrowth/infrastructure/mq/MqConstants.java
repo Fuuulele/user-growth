407dcb7e916b37e713100c38f3ca08f27958c863
TianTC <3042752220@qq.com>
2026-03-19 13:49:19 +0800

    MQ常量定义

---
+++ user-growth-infrastructure/src/main/java/com/usergrowth/infrastructure/mq/MqConstants.java
@@ -0,0 +1,13 @@
+package com.usergrowth.infrastructure.mq;
+
+public class MqConstants {
+
+    /** 兑换 Topic */
+    public static final String EXCHANGE_TOPIC = "user-growth-exchange-topic";
+
+    /** 兑换 Tag */
+    public static final String EXCHANGE_TAG = "exchange";
+
+    /** 消费者组 */
+    public static final String EXCHANGE_CONSUMER_GROUP = "user-growth-exchange-consumer-group";
+}
