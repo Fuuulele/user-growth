b1e300b05210d0473e479c30e883a7f5713b1df6
TianTC <3042752220@qq.com>
2026-03-20 09:37:59 +0800

    新增查询兑换结果方法

--- user-growth-application/src/main/java/com/usergrowth/application/service/AwardService.java
+++ user-growth-application/src/main/java/com/usergrowth/application/service/AwardService.java
@@ -1,6 +1,8 @@
 package com.usergrowth.application.service;
 
 import com.usergrowth.api.dto.AwardVO;
+import com.usergrowth.api.dto.ExchangeResultVO;
+
 import java.util.List;
 
 public interface AwardService {
@@ -8,5 +10,10 @@ public interface AwardService {
     List<AwardVO> getRewardList();
     boolean exchange(Long userId, Long awardId);
     boolean exchangeAsync(Long userId, Long awardId);
+
+    /**
+     * 查询兑换结果
+     */
+    ExchangeResultVO getExchangeResult(Long exchangeId);
 
 }
